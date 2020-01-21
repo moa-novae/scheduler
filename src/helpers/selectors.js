efceafe3bfda3b09bec5a13329be800635735235
Pumpkin-spice-cloud <alan_mo@live.com>
2020-01-21 17:57:46 -0500

    adds helper function selecting interview from a day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,17 @@
+export function getAppointmentsForDay (state, day) {
+  let filteredDay = state.days.filter(entry => entry.name === day)
+  let appointmentList = filteredDay[0] ? filteredDay[0].appointments : null;
+  //console.log(filteredDay[0].appointments)
+  let output = [];
+  if (appointmentList) {
+    for (let appointment in state.appointments) {
+
+      if (appointmentList.some((day) => day === state.appointments[appointment].id)) {
+        output.push(state.appointments[appointment]);
+      }
+    }
+  }
+  console.log(output)
+  return output;
+}
+
